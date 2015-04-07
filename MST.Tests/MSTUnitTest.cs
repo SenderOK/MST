ce7af41ff8fa18ab088bc3faee05a02ebd358935
Senderovich Nikita <niksettler@gmail.com>
2015-04-07 17:50:31 +0300

    Init

---
+++ MST.Tests/MSTUnitTest.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+using NUnit;
+using NUnit.Framework;
+
+using MST;
+
+namespace MST.Tests
+{    
+    [TestFixture]
+    public class TestClass
+    {
+        static int RunMST(StreamReader input)
+        {
+            string[] s = input.ReadLine().Split(' ');
+            int n = Int32.Parse(s[0]);
+            int m = Int32.Parse(s[1]);
+
+            double[][] g = new double[n][];
+            for (int i = 0; i < n; ++i)
+            {
+                g[i] = new double[n];
+                for (int j = 0; j < n; ++j)
+                {
+                    g[i][j] = MST.Inf;
+                }
+            }
+
+            for (int i = 0; i < m; ++i)
+            {
+                s = input.ReadLine().Split(' ');
+                int x = Int32.Parse(s[0]) - 1,
+                    y = Int32.Parse(s[1]) - 1,
+                    w = Int32.Parse(s[2]);
+                g[x][y] = w;
+                g[y][x] = w;
+            }
+
+            MSTResults res = MST.FindMST(g);            
+            return Convert.ToInt32(res.Weight);
+        }
+
+        static int GetAnswer(StreamReader input)
+        {
+            string[] s = input.ReadLine().Split(' ');
+            return Int32.Parse(s[0]);
+        }
+
+        static StreamReader GetReader(string InputFilename)
+        {
+            var assembly = Assembly.GetExecutingAssembly();            
+            string ResourceName = "MST.Tests.Resources." + InputFilename;
+            Stream s = assembly.GetManifestResourceStream(ResourceName);
+            return new StreamReader(s);
+        }
+
+        [TestCase]
+        public void Test01()
+        {
+            int AnsMST = RunMST(GetReader("Input01.txt"));
+            
+            int AnsCorr = GetAnswer(GetReader("Answer01.txt"));
+            Assert.AreEqual(AnsCorr, AnsMST);
+        }
+
+        [TestCase]
+        public void Test02()
+        {
+            int AnsMST = RunMST(GetReader("Input02.txt"));
+            int AnsCorr = GetAnswer(GetReader("Answer02.txt"));
+            Assert.AreEqual(AnsCorr, AnsMST);
+        }
+
+        [TestCase]
+        public void Test03()
+        {
+            int AnsMST = RunMST(GetReader("Input03.txt"));
+            int AnsCorr = GetAnswer(GetReader("Answer03.txt"));
+            Assert.AreEqual(AnsCorr, AnsMST);
+        }
+
+        [TestCase]
+        public void Test04()
+        {
+            int AnsMST = RunMST(GetReader("Input04.txt"));
+            int AnsCorr = GetAnswer(GetReader("Answer04.txt"));
+            Assert.AreEqual(AnsCorr, AnsMST);
+        }
+
+        [TestCase]
+        public void Test05()
+        {
+            int AnsMST = RunMST(GetReader("Input05.txt"));
+            int AnsCorr = GetAnswer(GetReader("Answer05.txt"));
+            Assert.AreEqual(AnsCorr, AnsMST);
+        }
+
+
+        [TestCase]
+        public void Test06()
+        {
+            int AnsMST = RunMST(GetReader("Input06.txt"));
+            int AnsCorr = GetAnswer(GetReader("Answer06.txt"));
+            Assert.AreEqual(AnsCorr, AnsMST);
+        }
+
+        [TestCase]
+        public void Test07()
+        {
+            int AnsMST = RunMST(GetReader("Input07.txt"));
+            int AnsCorr = GetAnswer(GetReader("Answer07.txt"));
+            Assert.AreEqual(AnsCorr, AnsMST);
+        }
+
+        [TestCase]
+        public void Test08()
+        {
+            int AnsMST = RunMST(GetReader("Input08.txt"));
+            int AnsCorr = GetAnswer(GetReader("Answer08.txt"));
+            Assert.AreEqual(AnsCorr, AnsMST);
+        }
+
+        [TestCase]
+        public void Test09()
+        {
+            int AnsMST = RunMST(GetReader("Input09.txt"));
+            int AnsCorr = GetAnswer(GetReader("Answer09.txt"));
+            Assert.AreEqual(AnsCorr, AnsMST);
+        }
+
+        [TestCase]
+        public void Test10()
+        {
+            int AnsMST = RunMST(GetReader("Input10.txt"));
+            int AnsCorr = GetAnswer(GetReader("Answer10.txt"));
+            Assert.AreEqual(AnsCorr, AnsMST);
+        }
+
+
+    }    
+
+}
---
+++ MST/MST.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MST
+{
+    public static class MST
+    {
+        public const double Inf = 1e150;
+
+        // takes on the input symmetric positive weight matrix g
+        public static MSTResults FindMST(double[][] g)
+        {
+            // preparing variables
+            int n = g.Length;
+            double[] minDist = new double[n];
+            int[] minNode = new int[n];
+            bool[] addedNodes = new bool[n];
+
+            // preparing result
+            List<List<int>> tree = new List<List<int>>();
+            for (int i = 0; i < n; ++i)
+            {
+                tree.Add(new List<int>());
+            }
+            double weight = 0;
+
+            // at first only 0 node is in the tree
+            for (int i = 0; i < n; ++i)
+            {
+                minDist[i] = g[i][0];
+                minNode[i] = 0;
+                addedNodes[i] = false;
+            }
+            //minDist[0] = Inf;
+            addedNodes[0] = true;
+            
+            for (int i = 0; i < n - 1; ++i)
+            {
+                // finding closest node
+                double currMinDist = Inf;
+                int currMinNode = 0;
+                for (int j = 0; j < n; ++j)
+                {
+                    //if (minDist[j] < currMinDist)
+                    if (!addedNodes[j] && minDist[j] < currMinDist)
+                    {
+                        currMinDist = minDist[j];
+                        currMinNode = j;
+                    }
+                }                
+
+                // adding the node to MST
+                weight += currMinDist;
+                //minDist[currMinNode] = Inf;
+                addedNodes[currMinNode] = true;
+                tree[currMinNode].Add(minNode[currMinNode]);
+                tree[minNode[currMinNode]].Add(currMinNode);
+
+                // recalculating minNodes
+                for (int j = 0; j < n; ++j)
+                {
+                    //if (minDist[j] < Inf && minDist[j] > g[j][currMinNode])
+                    if (!addedNodes[j] && minDist[j] > g[j][currMinNode])
+                    {
+                        minDist[j] = g[j][currMinNode];
+                        minNode[j] = currMinNode;
+                    }
+                }
+            }
+
+            return new MSTResults(tree, weight);
+        }
+    }
+
+    public class MSTResults
+    {
+        public List<List<int>> Tree { get; private set; }
+        public double Weight { get; private set; }
+
+        public MSTResults(List<List<int>> tree, double weight)
+        {
+            Tree = tree;
+            Weight = weight;
+        }
+    }
+}
